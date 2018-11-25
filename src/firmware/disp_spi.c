c3cf4ae1ab02fb826b89b0b9adee1d9523fab17f
Badger <badger@badgerhole.cz>
2018-11-25 14:24:47 +0100

    - more cleanup and defining hw conf by macros

--- src/firmware/disp_spi.c
+++ src/firmware/disp_spi.c
@@ -1,86 +1,73 @@
-#include "stdint.h"
-#include "disp_spi.h"
 
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/spi.h>
 
+#include <stdint.h>
 
+#include "hw_defs.h"
+#include "disp_spi.h"
 
+static void show(void);
 
 static void my_spi_init(void)
 {
 
-	rcc_periph_clock_enable(RCC_SPI1);
+	rcc_periph_clock_enable(DISP_COLS_SPI_RCC);
 
-	gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE,
-			GPIO3 | GPIO4 |  GPIO5);
-	gpio_set_af(GPIOB, GPIO_AF0, GPIO3 |GPIO4 | GPIO5);
+	gpio_mode_setup(DISP_COLS_CLK_GPIO, GPIO_MODE_AF, GPIO_PUPD_NONE,
+			DISP_COLS_CLK_GPIOP);
+	gpio_set_af(DISP_COLS_CLK_GPIO, DISP_COLS_CLK_AF_NUM,DISP_COLS_CLK_GPIOP);
 
-	/* Setup SPI parameters. */
-//	spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_4, SPI_CR1_CPOL,
-			//SPI_CR1_CPHA, SPI_CR1_MSBFIRST);
-	/* FIXME read from datasheed if rising all falling edge ofclock is better
-	 */
-	spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_4, 0,
+	gpio_mode_setup(DISP_COLS_DATA_GPIO, GPIO_MODE_AF, GPIO_PUPD_NONE,
+			DISP_COLS_DATA_GPIOP);
+	gpio_set_af(DISP_COLS_DATA_GPIO, DISP_COLS_DATA_AF_NUM,DISP_COLS_DATA_GPIOP);
+
+	spi_init_master(DISP_COLS_SPI, SPI_CR1_BAUDRATE_FPCLK_DIV_4, 0,
 			SPI_CR1_CPHA, SPI_CR1_MSBFIRST);
-	spi_enable_ss_output(SPI1); /* Required, see NSS, 25.3.1 section. */
-
+	spi_enable_ss_output(DISP_COLS_SPI);
 	/* Finally enable the SPI. */
-	spi_enable(SPI1);
-
+	spi_enable(DISP_COLS_SPI);
 }
 
 
-
-static void show(void);
 static void stcp_init(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_periph_clock_enable(RCC_GPIOB);
-	gpio_mode_setup(GPIOA, 
-		GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
-	
-			gpio_clear(GPIOA, GPIO15);
-			show();
+	gpio_mode_setup(DISP_COLS_STROBE_GPIO, 
+			GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
+			DISP_COLS_STROBE_GPIOP);
+
+	gpio_clear(DISP_COLS_STROBE_GPIO, DISP_COLS_STROBE_GPIOP);
+	show();
 }
 
 static void show(void) 
 {
-			gpio_set(GPIOA, GPIO15);
-//	for(dly = 0; dly < 500000; dly++);
-		__asm__("nop");
-		__asm__("nop");
-			gpio_clear(GPIOA, GPIO15);
-
+	gpio_set(DISP_COLS_STROBE_GPIO, DISP_COLS_STROBE_GPIOP);
+	__asm__("nop");
+	__asm__("nop");
+	gpio_clear(DISP_COLS_STROBE_GPIO, DISP_COLS_STROBE_GPIOP);
 }
 
 
 
 void disp_spi_col_set(uint8_t data)
 {
-//bitbang_send(data);
-//my_spi_send(data);
 
-spi_send(SPI1, data);
-/* wait for sending */
-while (SPI_SR(SPI1) & SPI_SR_BSY && !(SPI_SR(SPI1) & SPI_SR_TXE));
+	//bitbang_send(data);
+	spi_send(SPI1, data);
+	/* wait for sending */
+	while (SPI_SR(SPI1) & SPI_SR_BSY && !(SPI_SR(SPI1) & SPI_SR_TXE));
 
-/* FIXME make this with interrupt? */
-show();
+	/* FIXME make this with DMA and interrupt? */
+	show();
 }
 
 void disp_spi_init(void)
 {
 
-my_spi_init();
-//bitbang_init();
-stcp_init();
-#if 0
-#endif 
-//bitbang_send(0xCA);
-//show();
-//	for(dly = 0; dly < 10000000; dly++);
-//bitbang_send(0xAC);
-//show();
+	rcc_periph_clock_enable(DISP_COLS_GPIO_RCC);
+	my_spi_init();
+	//bitbang_init();
+	stcp_init();
 }
