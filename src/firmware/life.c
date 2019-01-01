79bf47fa476b9c1af2af8a186db7c9c9b3a4e410
Matthias Welwarsky <matze@welwarsky.de>
2019-01-01 15:54:23 +0100

    life: major update
    - new protocol for edge updates to create a namespace for control messages
    - fix major bug in tile sync protocol, sync is now realiable
    - add output of trace information through cdcacm function
    - depending on which edges are adjacent to each other, state messaages
      from the neighboring tiles need to be reversed to match coordinate
      systems
    - WIP: implement tick interval change through cdcacm

--- src/firmware/life.c
+++ src/firmware/life.c
@@ -11,11 +11,13 @@
 #include "ticker.h"
 #include "rand.h"
 #include "usart_buffered.h"
+#include "cdcacm.h"
 
 #define BOARD_HEIGHT 8
 #define BOARD_WIDTH 8
 
 #define LIFE_TICK 150
+static int life_tick_interv = LIFE_TICK;
 static unsigned int life_tick_next;
 static int seeding;
 static int life_generation;
@@ -33,6 +35,13 @@ static enum worker_state worker_state;
 #define HIGHLIFE	1
 #define MONOCHROME	0
 
+#define MSG_TYPE_MASK 0x80
+#define MSG_TYPE_EDGE 0x00
+#define MSG_TYPE_TICK 0x80
+#define MSG_SEQ_MASK  0x40
+#define MSG_SEQ_FIRST 0x00
+#define MSG_SEQ_SECND 0x40
+
 #if MONOCHROME
 #define LIFE_CELL(i, j) (31)
 #else
@@ -48,25 +57,41 @@ enum board_edge {
 	LEFT_EDGE,
 	NUM_EDGES
 };
-static int usart[NUM_EDGES] = {
-		USART_DIR_UP, USART_DIR_RIGHT, USART_DIR_DOWN, USART_DIR_LEFT
+
+static struct {
+	int num;
+	int invert;
+} usart[NUM_EDGES] = {
+		{ USART_DIR_UP, 0 },
+		{ USART_DIR_RIGHT, 0 },
+		{ USART_DIR_DOWN, 0 },
+		{ USART_DIR_LEFT, 0 }
 };
 
-void life_init(void)
+static const int edge_invert[NUM_EDGES][NUM_EDGES] = {
+		{ 1, 1, 0, 0 },
+		{ 1, 1, 0, 0 },
+		{ 0, 0, 1, 1 },
+		{ 0, 0, 1, 1 },
+};
+
+/**
+ * bitwise reverse
+ */
+static uint8_t revers8bit(uint8_t x)
 {
-	memset(board, 0, sizeof(board));
-	rand_init();
-	seeding = 12;
+	unsigned char b = x;
+	b = ((b * 0x0802LU & 0x22110LU) | (b * 0x8020LU & 0x88440LU)) * 0x10101LU >> 16;
+	return b;
+}
 
-	gpio_mode_setup(GPIOB,
-		GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO8|GPIO9);
-	gpio_set_output_options(GPIOB,
-			GPIO_OTYPE_OD, GPIO_OSPEED_LOW, GPIO8|GPIO9);
-	gpio_clear(GPIOB, GPIO8|GPIO9);
+static uint32_t get_final_char(int u)
+{
+	uint32_t last = (uint32_t)-1, curr;
 
-	worker_state = WAIT_TICK;
-	life_tick_next = ticker_get_ticks() + LIFE_TICK;
-	gpio_clear(GPIOF, GPIO0);
+	while ((curr = get_char(u)) != (uint32_t)-1)
+		last = curr;
+	return last;
 }
 
 static int board_get(int col, int row)
@@ -86,6 +111,20 @@ static void board_clean(void)
 			board_set(col, row, 0);
 		}
 	}
+	cdcacm_write_char('C');
+}
+
+static void send_edge_msg(int u, uint8_t edge_state)
+{
+	uint8_t buf[2];
+
+	buf[0] = MSG_TYPE_EDGE|MSG_SEQ_FIRST;
+	buf[0] |= edge_state >> 2;
+	buf[1] = MSG_TYPE_EDGE|MSG_SEQ_SECND;
+	buf[1] |= edge_state & 0x03;
+
+	send_char(u, buf[0]);
+	send_char(u, buf[1]);
 }
 
 static void transmit_edges(void)
@@ -116,42 +155,72 @@ static void transmit_edges(void)
 		default:
 			break;
 		}
-		send_char(usart[edge], edge_state);
-
+		send_edge_msg(usart[edge].num, edge_state);
 	}
 }
 
-static void receive_edges(void)
+static void update_edge(enum board_edge edge, uint8_t edge_state, int invert)
+{
+	if (invert)
+		edge_state = revers8bit(edge_state);
+
+	switch (edge) {
+	case TOP_EDGE:
+		for (int col = 0; col < BOARD_WIDTH; col++)
+			board_set(col, -1, (edge_state & (1<<col)) ? 8 : 0);
+		break;
+	case BOTTOM_EDGE:
+		for (int col = 0; col < BOARD_WIDTH; col++)
+			board_set(col, BOARD_HEIGHT, (edge_state & (1<<col)) ? 8 : 0);
+		break;
+	case LEFT_EDGE:
+		for (int row = 0; row < BOARD_HEIGHT; row++)
+			board_set(-1, row, (edge_state & (1<<row)) ? 8 : 0);
+		break;
+	case RIGHT_EDGE:
+		for (int row = 0; row < BOARD_HEIGHT; row++)
+			board_set(BOARD_WIDTH, row, (edge_state & (1<<row)) ? 8 : 0);
+		break;
+	default:
+		break;
+	}
+}
+
+
+static void receive_msg(void)
 {
 	enum board_edge edge;
 	uint32_t edge_state;
+	uint32_t buf;
 
 	for (edge = TOP_EDGE; edge < NUM_EDGES; edge++) {
-		edge_state = get_char(usart[edge]);
-		if (edge_state == (uint32_t)-1)
-			edge_state = 0;
+		edge_state = 0;
 
-		switch (edge) {
-		case TOP_EDGE:
-			for (int col = 0; col < BOARD_WIDTH; col++)
-				board_set(col, -1, (edge_state & (1<<col)) ? 8 : 0);
-			break;
-		case BOTTOM_EDGE:
-			for (int col = 0; col < BOARD_WIDTH; col++)
-				board_set(col, BOARD_HEIGHT, (edge_state & (1<<col)) ? 8 : 0);
-			break;
-		case LEFT_EDGE:
-			for (int row = 0; row < BOARD_HEIGHT; row++)
-				board_set(-1, row, (edge_state & (1<<row)) ? 8 : 0);
-			break;
-		case RIGHT_EDGE:
-			for (int row = 0; row < BOARD_HEIGHT; row++)
-				board_set(BOARD_WIDTH, row, (edge_state & (1<<row)) ? 8 : 0);
-			break;
-		default:
-			break;
+		while ((buf = get_char(usart[edge].num)) != (uint32_t)-1) {
+			if ((buf & MSG_TYPE_MASK) == MSG_TYPE_EDGE) {
+				if ((buf & MSG_SEQ_MASK) == MSG_SEQ_FIRST) {
+					edge_state |= (buf & 0x3f) << 2;
+				} else
+				if ((buf & MSG_SEQ_MASK) == MSG_SEQ_SECND) {
+					edge_state |= buf & 0x03;
+					update_edge(edge, edge_state, usart[edge].invert);
+				}
+			} else
+			if ((buf & MSG_TYPE_MASK) == MSG_TYPE_TICK) {
+				life_tick_interv = (buf & 0x7f) << 2;
+			}
 		}
 	}
+}
+
+static void update_life_tick(void)
+{
+	enum board_edge edge;
+	uint8_t buf;
+
+	buf = MSG_TYPE_TICK | (life_tick_interv >> 2);
+	for (edge = TOP_EDGE; edge < NUM_EDGES; edge++)
+		send_char(usart[edge].num, buf);
 }
 
 static int adjacent_to(int i, int j)
@@ -185,12 +254,22 @@ static uint8_t clamp(int val)
 
 static void add_random_life(void)
 {
-	int row = rand_get() % 8;
-	int col = rand_get() % 8;
+	int row = rand_get() % BOARD_HEIGHT;
+	int col = rand_get() % BOARD_WIDTH;
 	uint8_t val = board_get(col, row);
 
 	if (val == 0)
 		board_set(col, row, 1);
+}
+
+static void show_board(void)
+{
+	for (int i = 0; i < BOARD_WIDTH; i++) {
+		for (int j = 0; j < BOARD_HEIGHT; j++) {
+			disp_set_noupdate(i, j, board_get(i, j));
+		}
+	}
+	disp_update_rows();
 }
 
 static void life_tick(void)
@@ -238,11 +317,7 @@ static void life_tick(void)
 		}
 	}
 
-	for (i=0; i<BOARD_WIDTH; i++) {
-		for (j=0; j<BOARD_HEIGHT; j++) {
-			disp_set(i, j, board_get(i, j));
-		}
-	}
+	show_board();
 
 	if (have_life == last_life) {
 		++life_unchanged;
@@ -251,7 +326,7 @@ static void life_tick(void)
 		last_life = have_life;
 	}
 
-	if (life_unchanged > 50) {
+	if (!have_life || life_unchanged > 50) {
 		if (!scheduled_cleanup)
 			scheduled_cleanup = 1;
 	}
@@ -260,10 +335,50 @@ static void life_tick(void)
 
 }
 
+static void init_edges(void)
+{
+	enum board_edge edge;
+	enum board_edge remote_edge;
+	uint8_t val;
+
+	for (edge = TOP_EDGE; edge < NUM_EDGES; edge++) {
+		val = get_final_char(usart[edge].num);
+		switch (val) {
+		case 'T':
+			remote_edge = TOP_EDGE;
+			break;
+		case 'B':
+			remote_edge = BOTTOM_EDGE;
+			break;
+		case 'L':
+			remote_edge = LEFT_EDGE;
+			break;
+		case 'R':
+			remote_edge = RIGHT_EDGE;
+			break;
+		default:
+			remote_edge = NUM_EDGES;
+			break;
+		}
+		if (remote_edge != NUM_EDGES)
+			usart[edge].invert = edge_invert[edge][remote_edge];
+	}
+}
+
 void life_worker(void)
 {
 	unsigned int tick = ticker_get_ticks();
+	uint32_t c;
 
+	c = cdcacm_get_char();
+	if (c != (uint32_t)-1) {
+		if (c == '+' && life_tick_interv < 1000)
+			life_tick_interv += 50;
+		if (c == '-' && life_tick_interv > 50)
+			life_tick_interv -= 50;
+
+		update_life_tick();
+	}
 	if (worker_state == WAIT_TICK) {
 		if ((int)(tick - life_tick_next) < 0)
 			return;
@@ -277,24 +392,28 @@ void life_worker(void)
 		/* if somebody else holds it down, wait */
 		while (gpio_get(GPIOB, GPIO8|GPIO9) == 0)
 			;
-
-		worker_state = WORKER_BUSY;
-
-		life_tick_next = ticker_get_ticks() + LIFE_TICK;
-
-/* set idle signal again */
+		/* wait a bit to make sure everybody got it */
+		ticker_msleep(2);
+		/* set idle signal again */
 		gpio_clear(GPIOB, GPIO8|GPIO9);
+
+		cdcacm_write_char('T');
 
 		/* toggle the LED */
 		gpio_toggle(GPIOF, GPIO0);
+
+		worker_state = WORKER_BUSY;
+
+		life_tick_next = ticker_get_ticks() + life_tick_interv;
 	}
 
 	if (worker_state == WORKER_BUSY) {
 
-		receive_edges();
+		receive_msg();
 
 		/* seed life */
 		while (seeding) {
+			cdcacm_write_char('S');
 			add_random_life();
 			--seeding;
 		}
@@ -305,7 +424,7 @@ void life_worker(void)
 		/* stasis or extinction detected */
 		if (scheduled_cleanup) {
 			if (scheduled_cleanup == 1) {
-				disp_clean();
+				board_clean();
 				seeding = 12;
 				life_generation = 0;
 				last_life = 0;
@@ -324,4 +443,33 @@ void life_worker(void)
 
 		worker_state = WAIT_TICK;
 	}
+}
+
+void life_init(void)
+{
+	memset(board, 0, sizeof(board));
+	rand_init();
+	seeding = 12;
+
+	for (int u=0; u < 4; u++)
+		usart_buf_clear(u);
+
+	gpio_mode_setup(GPIOB,
+		GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, GPIO8|GPIO9);
+	gpio_set_output_options(GPIOB,
+			GPIO_OTYPE_OD, GPIO_OSPEED_LOW, GPIO8|GPIO9);
+	gpio_clear(GPIOB, GPIO8|GPIO9);
+
+	send_char(usart[TOP_EDGE].num, 'T');
+	send_char(usart[BOTTOM_EDGE].num, 'B');
+	send_char(usart[LEFT_EDGE].num, 'L');
+	send_char(usart[RIGHT_EDGE].num, 'R');
+
+	ticker_msleep(100);
+
+	init_edges();
+
+	worker_state = WAIT_TICK;
+	life_tick_next = ticker_get_ticks() + life_tick_interv;
+	gpio_clear(GPIOF, GPIO0);
 }
